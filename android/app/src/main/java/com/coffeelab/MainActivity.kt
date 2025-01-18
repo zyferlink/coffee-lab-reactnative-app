cf87d1b9677e5f27c61920cacc067533d753482a
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-18 20:17:03 +0530

    config (dependency): Install react-navigation, screens & safe-area-context

--- android/app/src/main/java/com/coffeelab/MainActivity.kt
+++ android/app/src/main/java/com/coffeelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.coffeelab
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -13,10 +14,15 @@ class MainActivity : ReactActivity() {
    */
   override fun getMainComponentName(): String = "CoffeeLab"
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
 }
