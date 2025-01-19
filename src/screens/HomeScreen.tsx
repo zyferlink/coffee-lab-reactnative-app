c9941f8b5654726716599682fb15408ee55264b4
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-19 21:49:15 +0530

    feat (home-screen): Init home screen ui impl

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,25 @@
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+
+import { useStore } from '../store/store'
 
 const HomeScreen = () => {
+
+  const coffeeList = useStore((state: any) => state.CoffeeList);
+  const beanList = useStore((state: any) => state.beanList);
+
+  const [categories, setCategories] = useState([undefined]);
+  const [categoryIndex, setCategoryIndex] = useState({
+    index: 0,
+    category: categories[0],
+  });
+  const [searchText, setSearchText] = useState(undefined);
+  const [sortedCoffee, setSortedCoffee] = useState(undefined);
+
+
   return (
     <View>
-      <Text style={{color:"black"}}>HomeScreen</Text>
+      <Text style={{ color: "black" }}>HomeScreen</Text>
     </View>
   )
 }
