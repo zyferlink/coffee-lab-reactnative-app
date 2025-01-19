cd69cb5ce58aaf2744ee30aff1fb88d3a9e46488
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-19 21:53:14 +0530

    feat (home-screen): Impl get category function for the logic

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,12 +3,29 @@ import { StyleSheet, Text, View } from 'react-native'
 
 import { useStore } from '../store/store'
 
+const getCategoriesFromList = (coffeeList: any[]) => {
+  const categoryCounts: Record<string, number> = {};
+
+  coffeeList.forEach((item) => {
+    if (!categoryCounts[item.name]) {
+      categoryCounts[item.name] = 0;
+    } else {
+      categoryCounts[item.name]++;
+    }
+  });
+  // Create categories list with "All" at the start
+  const categories = ["All", ...Object.keys(categoryCounts)];
+  return categories;
+};
+
 const HomeScreen = () => {
 
   const coffeeList = useStore((state: any) => state.CoffeeList);
   const beanList = useStore((state: any) => state.beanList);
 
-  const [categories, setCategories] = useState([undefined]);
+  const [categories, setCategories] = useState(
+    getCategoriesFromList(coffeeList)
+  );
   const [categoryIndex, setCategoryIndex] = useState({
     index: 0,
     category: categories[0],
