1f04a3086dd05e8371a25789de3891e07ce28885
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-21 17:05:15 +0530

    feat (home-screen): Impl ui functions for search-bar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 
 import { useStore } from '../store/store'
-import { COLORS, FONT_FAMILY, FONT_SIZE, SPACING } from '../theme/theme';
 import HeaderBar from '../components/HeaderBar';
+import CustomIcon from '../components/CustomIcon';
+import { BORDER_RADIUS, COLORS, FONT_FAMILY, FONT_SIZE, SPACING } from '../theme/theme';
 
 const getCategoriesFromList = (coffeeList: any[]) => {
   const categoryCounts: Record<string, number> = {};
@@ -35,7 +36,7 @@ const HomeScreen = () => {
   const coffeeList = useStore((state: any) => state.CoffeeList);
   const beanList = useStore((state: any) => state.beanList);
 
-  const [searchText, setSearchText] = useState(undefined);
+  const [searchText, setSearchText] = useState("");
   const [categories, setCategories] = useState(
     getCategoriesFromList(coffeeList)
   );
@@ -61,16 +62,39 @@ const HomeScreen = () => {
         contentContainerStyle={styles.scrollViewFlex}>
         {/*APP HEADER*/}
         <HeaderBar
-        title={"Header"}
+          title={"Header"}
         />
         {/*TITLE TEXT*/}
         <Text
-        style={styles.titleText}>
+          style={styles.titleText}>
           Find the bean
           {"\n"}
           coffee for you
         </Text>
-
+        {/*SEARCH INPUT*/}
+        <View
+          style={styles.inputContainer}>
+          <TouchableOpacity
+            onPress={() => { }}>
+            <CustomIcon
+              name="search"
+              size={FONT_SIZE.size18}
+              color={
+                searchText.length > 0
+                  ? COLORS.primaryOrange
+                  : COLORS.primaryLightGrey
+              }
+              style={styles.inputIcon}
+            />
+          </TouchableOpacity>
+          <TextInput
+            placeholder={"Find your Coffee..."}
+            value={searchText}
+            onChangeText={text => setSearchText(text)}
+            placeholderTextColor={COLORS.primaryLightGrey}
+            style={styles.textInputContainer}
+          />
+        </View>
       </ScrollView>
     </View>
   )
@@ -84,12 +108,31 @@ const styles = StyleSheet.create({
   scrollViewFlex: {
     flexGrow: 1,
   },
-  titleText:{
+  titleText: {
     fontSize: FONT_SIZE.size28,
     fontFamily: FONT_FAMILY.poppinsSemiBold,
     color: COLORS.primaryWhite,
     paddingLeft: SPACING.space30,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    margin: SPACING.space30,
+    borderRadius: BORDER_RADIUS.radius20,
+    backgroundColor: COLORS.primaryDarkGrey,
+    alignItems: "center",
+  },
+  inputIcon: {
+    marginHorizontal: SPACING.space20,
+
+  },
+  textInputContainer: {
+    flex: 1,
+    height: SPACING.space20 * 3,
+    fontFamily: FONT_FAMILY.poppinsMedium,
+    fontSize: FONT_SIZE.size14,
+    color: COLORS.primaryWhite,
   }
+
 })
 
 export default HomeScreen
