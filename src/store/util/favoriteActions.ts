b16414f78f4a928a176882e9d62fb1dfda8c856a
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-22 18:15:32 +0530

    feat (custom-hook): Impl use-store hook main functions

--- src/store/util/favoriteActions.ts
+++ src/store/util/favoriteActions.ts
@@ -1,23 +1,33 @@
 import produce from "immer";
 
 export const addToFavoriteList = (state: any, type: string, id: string) => {
-  const list = (type === "Coffee" ? state.coffeeList : state.beanList);
-  const item = list.find((item: any) => item.id === id);
-  if (item && !item.favorite) {
-    item.favorite = true;
-    state.favoriteList.unshift(item);
+  const list = type === "Coffee" ? state.coffeeList : state.beanList;
+  const index = list.findIndex((item: any) => item.id === id);
+
+  if (index !== -1) {
+      const item = list[index];
+
+      if (!item.favorite) {
+          item.favorite = true;
+          state.favoriteList.unshift(item);
+      }
   }
 };
 
 export const deleteFromFavoriteList = (state: any, type: string, id: string) => {
   const list = type === "Coffee" ? state.coffeeList : state.beanList;
-  const item = list.find((item: any) => item.id === id);
-  if (item && item.favorite) {
-    item.favorite = false;
-  }
+    const indexInList = list.findIndex((item: any) => item.id === id);
 
-  const index = state.favoriteList.findIndex((fav: any) => fav.id === id);
-  if (index !== -1) {
-    state.favoriteList.splice(index, 1);
-  }
+    if (indexInList !== -1) {
+        const item = list[indexInList];
+        if (item.favorite) {
+            item.favorite = false;
+        }
+    }
+
+    const indexInFavorites = state.favoriteList.findIndex((item: any) => item.id === id);
+
+    if (indexInFavorites !== -1) {
+        state.favoriteList.splice(indexInFavorites, 1);
+    }
 };
