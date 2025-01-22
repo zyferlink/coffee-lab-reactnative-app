1b61abf3f7fcc94127cefee5b269fb082f01cdc4
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-22 21:55:36 +0530

    feat (cart-screen): Init cart-screen

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useStore } from '../store/useStore';
 
 const CartScreen = () => {
+  const cartList = useStore((state: any) => state.cartList);
+  console.log("CartList size: ", cartList.length);
   return (
     <View>
       <Text>CartScreen</Text>
