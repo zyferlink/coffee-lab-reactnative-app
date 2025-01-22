38c9e64e614db1557e17e43d2f3828a14cdb2f3c
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-23 02:42:49 +0530

    feat (cart-screen): Init cart screen ui functions

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,12 +1,14 @@
-import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { useStore } from '../store/useStore';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { COLORS, SPACING } from '../theme/theme';
 import HeaderBar from '../components/HeaderBar';
 import EmptyListAnimation from '../components/EmptyListAnimation';
+import PaymentFooter from '../components/PaymentFooter';
+import CartItem from '../components/CartItem';
 
-const CartScreen = () => {
+const CartScreen = ({ navigation, route }: any) => {
   const tabBarHeight = useBottomTabBarHeight();
 
   const cartList = useStore((state: any) => state.cartList);
@@ -20,6 +22,10 @@ const CartScreen = () => {
   const addToOrderHistoryFromCart = useStore(
     (state: any) => state.addToOrderHistoryFromCart
   );
+
+  const buttonPressHandler = () => {
+    navigation.push("Payments");
+  }
 
   return (
     <View style={styles.screenContainer}>
@@ -37,19 +43,42 @@ const CartScreen = () => {
             <HeaderBar title={"Cart"} />
             {/* Cart Items */}
             {cartList.length == 0 ?
+              (<EmptyListAnimation title={"Cart is Empty!"} />)
+              :
+              (<View style={styles.listItemContainer}>
+                {cartList.map((item: any) => (
+                  <TouchableOpacity
+                    key={item.id}
+                    onPress={() => { }}>
+                    <CartItem
+                      id={item.id}
+                      index={item.index}
+                      name={item.name}
+                      type={item.type}
+                      roasted={item.roasted}
+                      imageLinkSquare={item.imageLinkSquare}
+                      specialIngredient={item.specialIngredient}
+                      prices={item.prices}
+                      incrementQuantityHandler={() => { }}
+                      decrementQuantityHandler={() => { }}
+                    />
+                  </TouchableOpacity>
+                ))}
+              </View>
+              )}
+          </View>
+          {
+            cartList.length != 0 ?
               (
-                <EmptyListAnimation
-                  title={"Cart is Empty!"}
+                <PaymentFooter
+                  buttonTitle={"Pay"}
+                  price={{ price: cartPrice, currency: "$" }}
+                  buttonPressHandler={buttonPressHandler}
                 />
               )
               :
-              (
-                <View style={styles.listItemContainer}>
-
-                </View>
-              )
-            }
-          </View>
+              (<></>)
+          }
 
         </View>
 
