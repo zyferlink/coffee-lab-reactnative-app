1ef4bfa405eb65feaf1a75f7b87bbea20a83e8d1
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-23 15:18:52 +0530

    feat (component/cart-item): Impl main ui functions

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,7 +1,8 @@
-import { Image, ImageProps, StyleSheet, Text, View } from 'react-native'
+import { Image, ImageProps, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import LinearGradient from 'react-native-linear-gradient';
 import { BORDER_RADIUS, COLORS, FONT_FAMILY, FONT_SIZE, SPACING } from '../theme/theme';
+import CustomIcon from './CustomIcon';
 
 interface CartItemProps {
     id: string;
@@ -36,13 +37,16 @@ const CartItem: React.FC<CartItemProps> = ({
                     end={{ x: 1, y: 1 }}
                     colors={[COLORS.primaryGrey, COLORS.primaryBlack]}
                     style={styles.cardItemlinearGradient}>
-                    {/* Top Row */}
+                    {/* Header Row */}
                     <View style={styles.cartItemRow}>
+                        {/* Image */}
                         <Image
                             source={imageLinkSquare}
                             style={styles.cartItemImage}
                         />
+                        {/* Main Property Views */}
                         <View style={styles.cartItemInfo}>
+                            {/* Title Views */}
                             <View>
                                 <Text style={styles.cartItemTitle}>
                                     {name}
@@ -51,7 +55,7 @@ const CartItem: React.FC<CartItemProps> = ({
                                     {specialIngredient}
                                 </Text>
                             </View>
-
+                            {/* Roasted Level Text */}
                             <View style={styles.cardItemRoastedContainer}>
                                 <Text style={styles.cardItemRoastedText}>
                                     {roasted}
@@ -59,6 +63,63 @@ const CartItem: React.FC<CartItemProps> = ({
                             </View>
                         </View>
                     </View>
+                    {/* Price Row */}
+                    {prices.map((price: any, index: any) => (
+                        <View
+                            key={index.toString()}
+                            style={styles.cartItemSizeRowContainer}>
+                            {/* Size List */}
+                            <View style={styles.cartItemSizeValueContainer}>
+                                <View style={styles.sizeBox}>
+                                    {/* Size Items */}
+                                    <Text
+                                        style={[styles.sizeText,
+                                        {
+                                            fontSize: type == "Bean" ?
+                                                FONT_SIZE.size14 : FONT_SIZE.size16
+                                        }]}>
+                                        {price.size}
+                                    </Text>
+                                </View>
+                                {/* Price Text */}
+                                <Text style={styles.sizeCurrency}>
+                                    {price.currency}
+                                    {" "}
+                                    <Text style={styles.sizePrice}>
+                                        {price.price}
+                                    </Text>
+                                </Text>
+                            </View>
+                            {/* Action Items */}
+                            <View style={styles.cartItemSizeValueContainer}>
+                                {/* Minus Button */}
+                                <TouchableOpacity
+                                    style={styles.cartItemIcon}>
+                                    <CustomIcon
+                                        name="minus"
+                                        color={COLORS.primaryWhite}
+                                        size={FONT_SIZE.size10}
+                                    />
+                                </TouchableOpacity>
+                                {/* Qantity Text */}
+                                <View style={styles.cartItemQuantityContainer}>
+                                    <Text style={styles.cartItemQuantityText}>
+                                        {price.quantity}
+                                    </Text>
+                                </View>
+                                {/* Add Button */}
+                                <TouchableOpacity
+                                    style={styles.cartItemIcon}>
+                                    <CustomIcon
+                                        name="add"
+                                        color={COLORS.primaryWhite}
+                                        size={FONT_SIZE.size10}
+                                    />
+                                </TouchableOpacity>
+                            </View>
+
+                        </View>
+                    ))}
                 </LinearGradient>
                 :
                 <></>
@@ -111,7 +172,62 @@ const styles = StyleSheet.create({
         fontFamily: FONT_FAMILY.poppinsRegular,
         fontSize: FONT_SIZE.size12,
         color: COLORS.primaryWhite,
-    }
+    },
+    cartItemSizeRowContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: SPACING.space20,
+        flexDirection: "row",
+    },
+    cartItemSizeValueContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
+    },
+    sizeBox: {
+        height: 40,
+        width: 100,
+        backgroundColor: COLORS.primaryBlack,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: BORDER_RADIUS.radius10,
+    },
+    sizeText: {
+        fontFamily: FONT_FAMILY.poppinsMedium,
+        color: COLORS.secondaryLightGrey,
+    },
+    sizeCurrency: {
+        fontFamily: FONT_FAMILY.poppinsSemiBold,
+        fontSize: FONT_SIZE.size18,
+        color: COLORS.primaryOrange,
+    },
+    sizePrice: {
+        fontFamily: FONT_FAMILY.poppinsSemiBold,
+        fontSize: FONT_SIZE.size18,
+        color: COLORS.primaryWhite,
+    },
+    cartItemIcon: {
+        backgroundColor: COLORS.primaryOrange,
+        padding: SPACING.space12,
+        borderRadius: BORDER_RADIUS.radius10,
+    },
+    cartItemQuantityContainer: {
+        backgroundColor: COLORS.primaryBlack,
+        width:60,
+        borderRadius: BORDER_RADIUS.radius10,
+        borderWidth:2,
+        borderColor: COLORS.primaryOrange,
+        alignItems: "center",
+        paddingVertical: SPACING.space4,
+        marginHorizontal: SPACING.space4,
+    },
+    cartItemQuantityText: {
+        fontFamily: FONT_FAMILY.poppinsSemiBold,
+        fontSize: FONT_SIZE.size16,
+        color: COLORS.primaryWhite,
+    },
 })
 
 export default CartItem
