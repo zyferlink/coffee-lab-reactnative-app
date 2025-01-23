c62a687e202f241fe3960c76119b54dcf19095ec
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-23 20:05:26 +0530

    feat (payment-screen): Init payment screen ui functions

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,14 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { COLORS } from '../theme/theme';
+
+const paymentList = [
+  {
+    name: "Wallet",
+    icon: "icon",
+    isIcon: true,
+  },
+  {
+    name: "Google Pay",
+    icon: require("../assets/app_images/gpay.png"),
+    isIcon: false,
+  },
+  {
+    name: "Apple Pay",
+    icon: require("../assets/app_images/applepay.png"),
+    isIcon: false,
+  },
+  {
+    name: "Amazon Pay",
+    icon: require("../assets/app_images/amazonpay.png"),
+    isIcon: false,
+  },
+];
 
 const PaymentScreen = () => {
+
+  const [paymentMode, setpaymentMode] = useState("Credit Card")
+
   return (
-    <View>
-      <Text>PaymentScreen</Text>
+    <View style={styles.screenContainer}>
+      {/* Status Bar */}
+      <StatusBar backgroundColor={COLORS.primaryBlack} />
+      {/* Scrollable Content */}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollViewFlex}>
+          {/* Header Content */}
+          <View style={styles.headerContainer}>
+
+          </View>
+
+      </ScrollView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlack,
+  },
+  scrollViewFlex: {
+    flexGrow: 1,
+  },
+  headerContainer:{},
+})
 
 export default PaymentScreen
