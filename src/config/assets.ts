c8284fb877b7de1408d5bb7d69613b325eaeeb75
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-25 18:03:28 +0530

    refactor (assets): Add assets constant for manage resources

---
+++ src/config/assets.ts
@@ -0,0 +1,16 @@
+const images = {
+    logoAmazonpay: require('../assets/app_images/amazonpay.png'),
+  };
+  
+  const icons = {
+    search: require('../assets/icons/ic_launcher.png'),
+  };
+  
+  const lottieAnimations = {
+    coffeecup: require('../assets/lottie/coffeecup.json'),
+    download: require('../assets/lottie/download.json'),
+    successful: require('../assets/lottie/successful.json'),
+  };
+  
+  export { images, icons, lottieAnimations };
+  
