896b2c882c7737b21d9c98926f5ee746559c3f0c
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-25 23:50:41 +0530

    refactor (navigation): Update tab-navigator and add routes

---
+++ src/navigation/routes.ts
@@ -0,0 +1,14 @@
+export const SCREENS = {
+    HOME: 'Home',
+    CART: 'Cart',
+    FAVORITE: 'Favorite',
+    HISTORY: 'History',
+    DETAIL: 'Detail',
+    PAYMENT: 'Payment',
+  };
+  
+  export const NAVIGATORS = {
+    ROOT_STACK: 'RootStackNavigator',
+    TAB: 'TabNavigator',
+  };
+  
