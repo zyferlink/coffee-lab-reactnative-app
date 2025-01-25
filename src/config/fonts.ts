b930fd6d10465f9d0e2bfa027ceb1ef68ee92186
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-25 20:45:20 +0530

    config (font-sizes): Add font sizes config for resources

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -27,3 +27,34 @@ export const fonts: Fonts = {
         thin: 'Poppins-Thin',
     },
 };
+
+
+//-----------------------------------
+
+interface FontSizes {
+    size8: number;
+    size10: number;
+    size12: number;
+    size14: number;
+    size16: number;
+    size18: number;
+    size20: number;
+    size24: number;
+    size28: number;
+    size30: number;
+    size32: number;
+}
+
+export const fontSizes: FontSizes = {
+    size8: 8,
+    size10: 10,
+    size12: 12,
+    size14: 14,
+    size16: 16,
+    size18: 18,
+    size20: 20,
+    size24: 24,
+    size28: 28,
+    size30: 30,
+    size32: 32,
+};
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -86,31 +86,31 @@ interface Spacing {
   //   poppinsThin: 'Poppins-Thin',
   // };
   
-  interface FontSize {
-    size8: number;
-    size10: number;
-    size12: number;
-    size14: number;
-    size16: number;
-    size18: number;
-    size20: number;
-    size24: number;
-    size28: number;
-    size30: number;
-  }
+  // interface FontSize {
+  //   size8: number;
+  //   size10: number;
+  //   size12: number;
+  //   size14: number;
+  //   size16: number;
+  //   size18: number;
+  //   size20: number;
+  //   size24: number;
+  //   size28: number;
+  //   size30: number;
+  // }
   
-  export const FONT_SIZE: FontSize = {
-    size8: 8,
-    size10: 10,
-    size12: 12,
-    size14: 14,
-    size16: 16,
-    size18: 18,
-    size20: 20,
-    size24: 24,
-    size28: 28,
-    size30: 30,
-  };
+  // export const FONT_SIZE: FontSize = {
+  //   size8: 8,
+  //   size10: 10,
+  //   size12: 12,
+  //   size14: 14,
+  //   size16: 16,
+  //   size18: 18,
+  //   size20: 20,
+  //   size24: 24,
+  //   size28: 28,
+  //   size30: 30,
+  // };
   
   interface BorderRadius {
     radius4: number;
