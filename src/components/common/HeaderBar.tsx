73e31b30f6c4ad8cb43dc28f8829cfd56581dc4a
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-28 21:02:06 +0530

    refactor (comp/header-bar): integrate nativewind and remove stylesheet

--- src/components/common/HeaderBar.tsx
+++ src/components/common/HeaderBar.tsx
@@ -1,47 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text } from 'react-native';
+import { fontSizes } from '../../config/fonts';
+import { colors } from '../../config/colors';
+import { iconSet } from '../../config/assets';
 import GradientIconBG from './GradientIconBG';
 import ProfilePicture from '../specific/ProfilePicture';
-import { fonts, fontSizes } from '../../config/fonts';
-import { colors } from '../../config/colors';
-import { spacing } from '../../config/dimensions';
 
 interface HeaderBarProps {
     title?: string;
 }
 
-const HeaderBar: React.FC<HeaderBarProps> =
-    ({ title }) => {
-        return (
-            <View
-                style={styles.headerContainer}>
-                <GradientIconBG
-                    name="menu"
-                    color={colors.primary.lightGrey}
-                    size={fontSizes.size12}
-                />
-                <Text
-                    style={styles.headerText}>
-                    {title}
-                </Text>
-                <ProfilePicture />
-            </View>
-        )
-    }
+const HeaderBar: React.FC<HeaderBarProps> = ({ title }) => {
+    return (
+        <View className="px-6 py-4 flex-row items-center justify-between">
+            <GradientIconBG
+                name={iconSet.menu}
+                color={colors.primary.lightGrey}
+                size={fontSizes.size12}
+            />
+            <Text className="text-2xl font-poppinsLight text-secondary-lightGrey">
+                {title}
+            </Text>
+            <ProfilePicture />
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({
-    headerContainer: {
-        paddingHorizontal: spacing.space24,
-        paddingVertical: spacing.space16,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-    },
-    headerText: {
-        fontFamily: fonts.poppins.light,
-        fontSize: fontSizes.size24,
-        color: colors.secondary.lightGrey,
-    }
-})
-
-export default HeaderBar
+export default HeaderBar;
