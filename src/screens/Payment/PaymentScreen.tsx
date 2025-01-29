757dd1ff698d27a2eec5406c8629e7e9e4fa93e8
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-29 17:53:13 +0530

    refactor (payment-screen): integrate nativewind and remove stylesheet

--- src/screens/Payment/PaymentScreen.tsx
+++ src/screens/Payment/PaymentScreen.tsx
@@ -1,22 +1,27 @@
-import { ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
+import { ScrollView, StatusBar, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
+import { useStore } from '../../state/useStore';
+import { iconSet, lottieAnimations } from '../../config/assets';
+import { fontSizes } from '../../config/fonts';
+import { colors } from '../../config/colors';
+import { PAYMENT_OPTIONS } from '../../data/paymentOptions';
+import { NAVIGATORS, SCREENS } from '../../config/screenNames';
+import { BUTTON_TITLES, CURRENCY, PAYMENT_METHODS } from '../../config/specialTypes';
 import GradientIconBG from '../../components/common/GradientIconBG';
 import PaymentMethod from './components/PaymentMethod';
 import PaymentFooter from '../../components/common/PaymentFooter';
 import LinearGradient from 'react-native-linear-gradient';
 import CustomIcon from '../../components/common/CustomIcon';
-import { useStore } from '../../state/useStore';
 import PopUpAnimation from '../../components/common/PopUpAnimation';
-import { iconSet, lottieAnimations } from '../../config/assets';
-import { fonts, fontSizes } from '../../config/fonts';
-import { colors } from '../../config/colors';
-import { borderRadius, spacing } from '../../config/dimensions';
-import { PAYMENT_OPTIONS } from '../../data/paymentOptions';
-import { NAVIGATORS, SCREENS } from '../../config/screenNames';
-import { BUTTON_TITLES, CURRENCY, PAYMENT_METHODS } from '../../config/specialTypes';
 
+interface PaymentScreenProps {
+  navigation: any;
+  route: any;
+}
 
-const PaymentScreen = ({ navigation, route }: any) => {
+const PaymentScreen: React.FC<PaymentScreenProps> = ({ navigation, route }) => {
 
   const calculateCartPrice = useStore((state: any) =>
     state.calculateCartPrice
@@ -39,63 +44,60 @@ const PaymentScreen = ({ navigation, route }: any) => {
   };
 
   return (
-    <View style={styles.screenContainer}>
+    <SafeAreaView className="flex-1 bg-primary-black">
       {/* Status Bar */}
       <StatusBar backgroundColor={colors.primary.black} />
       {/* Success Animation */}
-      {showSuccessAnimation ?
+      {showSuccessAnimation &&
         <PopUpAnimation
-        style={styles.lottieAnimation}
-        source={lottieAnimations.successful}
+          style={{ flex: 1 }}
+          source={lottieAnimations.successful}
         />
-        : <></>}
+      }
       {/* Scrollable Content */}
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.scrollViewFlex}>
+        contentContainerStyle={tw`flex-grow-1`}>
         {/* Header Content */}
-        <View style={styles.headerContainer}>
+        <View className="px-6 py-4 flex-row items-center justify-between">
           {/* Back Button */}
           <TouchableOpacity
             onPress={() => navigation.pop()}>
             <GradientIconBG
               name={iconSet.left}
-              color={colors.primary.lightGrey}
+              color={colors.secondary.lightGrey}
               size={fontSizes.size16}
             />
           </TouchableOpacity>
           {/* Header Text */}
-          <Text style={styles.headerText}>
+          <Text className="text-xl font-poppinsSemiBold text-primary-white">
             Payments
           </Text>
-          <View style={styles.emptyView} />
+          <View className="w-9" />
         </View>
         {/* Payment Option Content */}
-        <View style={styles.paymentOptionContainer}>
+        <View className="p-4 space-y-4">
           {/* Credit Card View */}
           <TouchableOpacity
             onPress={() => setPaymentMode(PAYMENT_METHODS.CREDIT_CARD)}>
             <View
-              style={[styles.paymentCardContainer, {
-                borderColor:
-                  paymentMode == PAYMENT_METHODS.CREDIT_CARD ?
-                  colors.primary.orange : colors.primary.grey
-              }]}>
+              className={`p-3 space-y-2 border border-2 rounded-3xl 
+              ${paymentMode === PAYMENT_METHODS.CREDIT_CARD ? 'border-primary-orange' : 'border-primary-lightGrey'}`}>
               {/* Credit Card Title */}
-              <Text style={styles.creditCardTitle}>
+              <Text className="text-sm font-poppinsSemiBold text-primary-white ml-2">
                 Credit Card
               </Text>
               {/* Credit Card Background */}
-              <View style={styles.creditCardBackground}>
+              <View className="bg-primary-darkGrey rounded-2xl">
                 <LinearGradient
                   start={{ x: 0, y: 0 }}
                   end={{ x: 1, y: 1 }}
                   colors={[colors.primary.grey, colors.primary.black]}
-                  style={styles.linearGradientCreditCard}>
-                  <View style={styles.creditCardRow}>
+                  className="rounded-2xl p-4 space-y-6">
+                  <View className="flex-row justify-between items-center">
                     {/* Credit Card Chip Icon */}
                     <CustomIcon
-                      name={iconSet.visa}
+                      name={iconSet.chip}
                       size={fontSizes.size20 * 2}
                       color={colors.primary.orange}
                     />
@@ -108,36 +110,36 @@ const PaymentScreen = ({ navigation, route }: any) => {
                   </View>
                   {/* Credit Card Number */}
                   <View
-                    style={styles.creditCardNumberContainer}>
-                    <Text style={styles.creditCardNumber}>
+                    className="flex-row space-x-2 items-center">
+                    <Text className="text-[22px] font-poppinsMedium text-primary-white tracking-[4]">
                       0101
                     </Text>
-                    <Text style={styles.creditCardNumber}>
+                    <Text className="text-[22px] font-poppinsMedium text-primary-white tracking-[4]">
                       0101
                     </Text>
-                    <Text style={styles.creditCardNumber}>
+                    <Text className="text-[22px] font-poppinsMedium text-primary-white tracking-[4]">
                       8855
                     </Text>
-                    <Text style={styles.creditCardNumber}>
+                    <Text className="text-[22px] font-poppinsMedium text-primary-white tracking-[4]">
                       3477
                     </Text>
                   </View>
                   {/* Credit Card Holder Info */}
-                  <View style={styles.creditCardRow}>
-                    <View style={styles.creditCardNameContainer}>
-                      <Text style={styles.creditCardNameTitle}>
+                  <View className="flex-row justify-between items-center">
+                    <View >
+                      <Text className="text-xs font-poppinsRegular text-secondary-lightGrey mb-0.5">
                         Card Holder Name
                       </Text>
-                      <Text style={styles.creditCardName}>
+                      <Text className="text-lg font-poppinsMedium text-primary-white">
                         Shavinda Nova
                       </Text>
                     </View>
                     {/* Credit Card Expiry Info */}
-                    <View style={styles.creditCardExpireContainer}>
-                      <Text style={styles.creditCardNameTitle}>
+                    <View className="items-end">
+                      <Text className="text-xs font-poppinsRegular text-secondary-lightGrey mb-0.5">
                         Expiry Date
                       </Text>
-                      <Text style={styles.creditCardName}>
+                      <Text className="text-lg font-poppinsMedium text-primary-white">
                         01/30
                       </Text>
                     </View>
@@ -163,98 +165,12 @@ const PaymentScreen = ({ navigation, route }: any) => {
       </ScrollView>
       {/* Payment Footer */}
       <PaymentFooter
-        buttonTitle={BUTTON_TITLES.PAY_WITH(paymentMode)} 
+        buttonTitle={BUTTON_TITLES.PAY_WITH(paymentMode)}
         price={{ price: route.params.amount, currency: CURRENCY.USD.symbol }}
-         priceContainerStyle={"py-7"}
+        priceContainerStyle={"py-7"}
         buttonPressHandler={buttonPressHandler} />
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    backgroundColor: colors.primary.black,
-  },
-  lottieAnimation:{
-    flex: 1,
-  },
-  scrollViewFlex: {
-    flexGrow: 1,
-  },
-  headerContainer: {
-    paddingHorizontal: spacing.space24,
-    paddingVertical: spacing.space16,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  headerText: {
-    fontFamily: fonts.poppins.semiBold,
-    fontSize: fontSizes.size20,
-    color: colors.primary.white,
-  },
-  emptyView: {
-    width: spacing.space36,
-  },
-  paymentOptionContainer: {
-    padding: spacing.space16,
-    gap: spacing.space16,
-  },
-  paymentCardContainer: {
-    padding: spacing.space10,
-    gap: spacing.space10,
-    borderRadius: borderRadius.radius16,
-    borderWidth: 3,
-  },
-  creditCardTitle: {
-    fontFamily: fonts.poppins.semiBold,
-    fontSize: fontSizes.size14,
-    color: colors.primary.white,
-    marginLeft: spacing.space10,
-  },
-  creditCardBackground: {
-    backgroundColor: colors.primary.grey,
-    borderRadius: borderRadius.radius28,
-  },
-  linearGradientCreditCard: {
-    borderRadius: borderRadius.radius20,
-    gap: spacing.space30,
-    paddingHorizontal: spacing.space16,
-    paddingVertical: spacing.space10,
-  },
-  creditCardRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  creditCardNumberContainer: {
-    flexDirection: "row",
-    gap: spacing.space10,
-    alignItems: "center",
-  },
-  creditCardNumber: {
-    fontFamily: fonts.poppins.semiBold,
-    fontSize: fontSizes.size20,
-    color: colors.primary.white,
-    letterSpacing: 6,
-  },
-  creditCardNameContainer: {
-    alignItems: "flex-start",
-  },
-  creditCardExpireContainer: {
-    alignItems: "flex-end",
-  },
-  creditCardNameTitle: {
-    fontFamily: fonts.poppins.regular,
-    fontSize: fontSizes.size14,
-    color: colors.secondary.lightGrey,
-  },
-  creditCardName: {
-    fontFamily: fonts.poppins.medium,
-    fontSize: fontSizes.size18,
-    color: colors.primary.white,
-  },
-})
-
-export default PaymentScreen
+export default PaymentScreen;
