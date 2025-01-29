5ae878b4b8c190b14978565f30596ca186cfd34c
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-29 21:36:03 +0530

    refactor (home-screen): mvvm migration

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,19 +1,14 @@
 // React and React Native
-import React, { useRef, useState } from 'react'
-import {
-  Animated, FlatList, ScrollView, StatusBar,
-  Text, ToastAndroid, TouchableOpacity, View
-} from 'react-native'
+import React from 'react'
+import { FlatList, StatusBar, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 // Configuration and components
+import { useHomeViewModel } from './HomeViewModel';
 import { colors } from '../../config/colors';
 import { SCREENS } from '../../config/screenNames';
-import { MESSAGES } from '../../config/messages';
 import { CONSTANTS } from '../../config/constants';
-import { useStore } from '../../state/useStore'
 import { Product } from '../../types/common/product';
-import { CartItem } from '../../types/common/cartItem';
 import HeaderBar from '../../components/common/HeaderBar';
 import ProductCard from '../../components/common/ProductCard';
 import SearchInput from './components/SearchInput';
@@ -25,66 +20,11 @@ interface HomeScreenProps {
 }
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
-  const listRef = useRef<FlatList<Product>>(null);
   const tabBarHeight = useBottomTabBarHeight();
-  const beanList = useStore((state: any) => state.beanList);
-  const addToCart = useStore((state: any) => state.addToCart);
-  const coffeeList = useStore((state: any) => state.coffeeList);
-  const calculateCartPrice = useStore((state: any) => state.calculateCartPrice);
-
-  const [searchText, setSearchText] = useState("");
-  const [categories, setCategories] = useState(getCategoriesFromList(coffeeList));
-  const [categoryIndex, setCategoryIndex] = useState({
-    index: 0,
-    category: categories[0],
-  });
-  const [sortedCoffee, setSortedCoffee] = useState(
-    getSortedCoffeeList(categoryIndex.category, coffeeList)
-  );
-
-  const handleCategoryChange = (index: number) => {
-    scrollToTop(listRef);
-    setCategoryIndex({
-      index: index,
-      category: categories[index]
-    });
-    setSortedCoffee([
-      ...getSortedCoffeeList(categories[index], coffeeList),
-    ]);
-  }
-
-  const searchCoffee = (search: string) => {
-    scrollToTop(listRef);
-    setCategoryIndex({
-      index: 0, category: categories[0]
-    });
-    setSortedCoffee([
-      ...coffeeList.filter((item: any) =>
-        item.name.toLowerCase().includes(search.toLowerCase()),
-      ),
-    ]);
-  };
-
-  const resetSearchCoffee = () => {
-    scrollToTop(listRef);
-    setCategoryIndex({
-      index: 0, category: categories[0]
-    });
-    setSortedCoffee([...coffeeList]);
-    setSearchText("");
-  };
-
-  const addToCartHandler = (
-    cartItem: CartItem,
-  ) => {
-    addToCart(cartItem);
-    calculateCartPrice();
-    ToastAndroid.showWithGravity(
-      MESSAGES.SUCCESS.ITEM_ADDED_TO_CART(cartItem.name),
-      ToastAndroid.SHORT,
-      ToastAndroid.CENTER
-    );
-  };
+  const {
+    listRef, searchText, setSearchText, categories, categoryIndex, sortedCoffee,
+    handleCategoryChange, searchCoffee, resetSearchCoffee, addToCartHandler, beanList,
+  } = useHomeViewModel();
 
   const renderCoffeeItem = ({ item }: { item: Product }) => (
     <TouchableOpacity
@@ -179,48 +119,5 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
     </SafeAreaView>
   );
 }
-
-// UTILITY FUNCTIONS
-// ->
-const getCategoriesFromList = (coffeeList: any[]) => {
-  const categoryCounts: Record<string, number> = {};
-
-  coffeeList.forEach((item) => {
-    if (!categoryCounts[item.name]) {
-      categoryCounts[item.name] = 0;
-    } else {
-      categoryCounts[item.name]++;
-    }
-  });
-  // Create categories list with "All" at the start
-  const categories = [CONSTANTS.GENERAL.CATEGORY_ALL, ...Object.keys(categoryCounts)];
-  return categories;
-};
-
-const getSortedCoffeeList = (seletedCategory: string, coffeeList: any[]) => {
-  if (seletedCategory == CONSTANTS.GENERAL.CATEGORY_ALL) return coffeeList;
-
-  const sortedList = coffeeList.filter(
-    (coffeeCategory: any) => coffeeCategory.name == seletedCategory
-  )
-  return sortedList;
-}
-
-const scrollToTop = (listRef: React.RefObject<any>) => {
-  if (listRef?.current) {
-    const scrollValue = new Animated.Value(0);
-
-    Animated.timing(scrollValue, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-
-    listRef.current.scrollToOffset({
-      animated: true,
-      offset: 0,
-    });
-  }
-};
 
 export default HomeScreen;
