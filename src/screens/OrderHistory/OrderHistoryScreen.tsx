ce5ee69ed8418693bb0b46a3f7ff5e55e0ec9cf0
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-30 01:12:43 +0530

    refactor (order-history-screen): mvvm migration

--- src/screens/OrderHistory/OrderHistoryScreen.tsx
+++ src/screens/OrderHistory/OrderHistoryScreen.tsx
@@ -1,52 +1,38 @@
-import React, { useState } from 'react'
-import { ScrollView, StatusBar, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react';
+import { ScrollView, StatusBar, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import tw from 'twrnc';
-import { useStore } from '../../state/useStore';
-import { lottieAnimations } from '../../config/assets';
 import { colors } from '../../config/colors';
-import { SCREENS } from '../../config/screenNames';
 import { CONSTANTS } from '../../config/constants';
 import { MESSAGES } from '../../config/messages';
 import HeaderBar from '../../components/common/HeaderBar';
 import EmptyListAnimation from '../../components/common/EmptyListAnimation';
 import PopUpAnimation from '../../components/common/PopUpAnimation';
 import OrderHistoryCard from './components/OrderHistoryCard';
+import { useOrderHistoryViewModel } from './OrderHistoryViewModel';
 
 interface OrderHistoryScreenProps {
   navigation: any;
 }
 
-const OrderHistoryScreen : React.FC<OrderHistoryScreenProps> = ({ navigation }) => {
+const OrderHistoryScreen: React.FC<OrderHistoryScreenProps> = ({ navigation }) => {
   const tabBarHeight = useBottomTabBarHeight();
-
-  const orderHistoryList = useStore((state: any) => state.orderHistoryList);
-
-  const [showAnimation, setShowAnimation] = useState(false)
-
-  const navigationHandler = ({ index, id, type }: any) => {
-    navigation.push(SCREENS.DETAIL, { index, id, type });
-  }
-
-  const downloadActionHandler = () => {
-    setShowAnimation(true);
-    setTimeout(() => {
-      setShowAnimation(false);
-    }, 1500);
-  }
+  const {
+    orderHistoryList, showAnimation, navigationHandler, downloadActionHandler, lottieAnimations
+  } = useOrderHistoryViewModel(navigation);
 
   return (
     <SafeAreaView className="flex-1 bg-primary-black">
       {/* Status Bar */}
       <StatusBar backgroundColor={colors.primary.black} />
       {/* Success Animation */}
-      {showAnimation &&
+      {showAnimation && (
         <PopUpAnimation
           style={{ height: 250 }}
           source={lottieAnimations.download}
         />
-      }
+      )}
       {/* Scrollable Content */}
       <ScrollView
         showsVerticalScrollIndicator={false}
@@ -54,15 +40,14 @@ const OrderHistoryScreen : React.FC<OrderHistoryScreenProps> = ({ navigation }) 
         <View
           className="flex-1 justify-between pb-4"
           style={{ marginBottom: tabBarHeight }}>
-          <View
-            className="flex-1">
+          <View className="flex-1">
             {/* Header Bar */}
             <HeaderBar title={CONSTANTS.TITLES.ORDER_HISTORY} />
             {/* Order History Items */}
-            {orderHistoryList.length == 0 ?
-              (<EmptyListAnimation title={MESSAGES.DEFAULTS.NO_ORDER_HISTORY} />)
-              :
-              (<View className="px-7 gap-5">
+            {orderHistoryList.length === 0 ? (
+              <EmptyListAnimation title={MESSAGES.DEFAULTS.NO_ORDER_HISTORY} />
+            ) : (
+              <View className="px-7 gap-5">
                 {orderHistoryList.map((orderItem: any, index: any) => (
                   <OrderHistoryCard
                     key={index.toString()}
@@ -71,12 +56,13 @@ const OrderHistoryScreen : React.FC<OrderHistoryScreenProps> = ({ navigation }) 
                   />
                 ))}
               </View>
-              )}
+            )}
           </View>
           {orderHistoryList.length > 0 && (
             <TouchableOpacity
               className="h-16 rounded-full mt-5 mx-7 bg-primary-orange items-center justify-center"
-              onPress={() => { downloadActionHandler(); }}>
+              onPress={downloadActionHandler}
+            >
               <Text className="font-poppinsSemiBold text-lg text-white">
                 Download
               </Text>
