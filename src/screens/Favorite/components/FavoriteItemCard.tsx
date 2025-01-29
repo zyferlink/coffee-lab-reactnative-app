f65889a2c34b01fc17a7afe3811dd6331c8eaa38
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-29 14:52:30 +0530

    refactor (comp/favorite-item-card): integrate nativewind and remove stylesheet

--- src/screens/Favorite/components/FavoriteItemCard.tsx
+++ src/screens/Favorite/components/FavoriteItemCard.tsx
@@ -1,11 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Text, View } from 'react-native'
+import { colors } from '../../../config/colors';
+import { Product } from '../../../types/common/product';
 import ImageBackdropInfo from '../../../components/common/ImageBackdropInfo';
 import LinearGradient from 'react-native-linear-gradient';
-import { fonts, fontSizes } from '../../../config/fonts';
-import { colors } from '../../../config/colors';
-import { borderRadius, spacing } from '../../../config/dimensions';
-import { Product } from '../../../types/common/product';
 
 interface FavoriteItemCardProps {
   product: Product;
@@ -17,7 +15,7 @@ const FavoriteItemCard: React.FC<FavoriteItemCardProps> = ({
   toggleFavorite,
 }) => {
   return (
-    <View style={styles.cardContainer}>
+    <View className="overflow-hidden rounded-3xl">
       <ImageBackdropInfo
         product={product}
         toggleFavorite={toggleFavorite}
@@ -29,40 +27,18 @@ const FavoriteItemCard: React.FC<FavoriteItemCardProps> = ({
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         colors={[colors.primary.grey, colors.primary.black]}
-        style={styles.linearGradientContainer}>
-        {/* Description Area */}
-        <Text style={styles.descriptionTitle}>
+        className="gap-1 p-2.5">
+        {/* Description Title */}
+        <Text className="font-poppinsSemiBold text-lg text-primary-white mb-1">
           Description
         </Text>
-        <Text style={styles.descriptionText}>
+        {/* Description Area */}
+        <Text className="font-poppinsRegular text-sm text-primary-white tracking-wide mb-2">
           {product.description}
         </Text>
       </LinearGradient>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  cardContainer: {
-    overflow: "hidden",
-    borderRadius: borderRadius.radius28,
-  },
-  linearGradientContainer: {
-    gap: spacing.space10,
-    padding: spacing.space10,
-  },
-  descriptionTitle: {
-    fontFamily: fonts.poppins.semiBold,
-    fontSize: fontSizes.size16,
-    color: colors.primary.white,
-    marginBottom: spacing.space4,
-  },
-  descriptionText: {
-    fontFamily: fonts.poppins.regular,
-    fontSize: fontSizes.size14,
-    color: colors.primary.white,
-    letterSpacing: 0.5,
-  },
-})
 
 export default FavoriteItemCard;
